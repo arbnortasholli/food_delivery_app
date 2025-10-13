3417362738d3d33a810c68dd0dc85566f6002893
AlbionHajzeraj <albion.hajzeraj@student.uni-pr.edu>
2025-10-13 17:49:23 +0200

    rregullim i nje errori

--- screens/loginscreen.js
+++ screens/loginscreen.js
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { TextInput } from 'react-native/types_generated/index'
+import { TextInput } from 'react-native'
 
 const loginscreen = () => {
   return (
