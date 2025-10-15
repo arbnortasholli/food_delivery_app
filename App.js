57b65a08b2cb2526832adb087be6c2436f82b4a2
Arber Halimi <arber.halimi2@student.uni-pr.edu>
2025-10-15 23:45:42 +0200

    app.js logjika

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from './context/ThemeContext';
+import { CartProvider } from './context/CartContext';
+import TabNavigator from './navigation/TabNavigator';
 
+export default function App() {
+  return (
+    <ThemeProvider>
+      <CartProvider>
+        <NavigationContainer>
+          <TabNavigator />
+        </NavigationContainer>
+      </CartProvider>
+    </ThemeProvider>
+  );
+}
